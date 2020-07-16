b5533e685a44689d9392f9d614ac7085910cd209
Dito Cahya Pratama <ditocahyapratama717@gmail.com>
2020-07-16 21:13:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
+import ListStudent from './pages/student/ListStudent';
+import EditStudent from './pages/student/EditStudent';
+import CreateStudent from './pages/student/CreateStudent';
+import DeleteStudent from './pages/student/DeleteStudent';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Link to="/" className="navbar-brand">School MERN App</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">List Student</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Create Student</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <br />
+        <Route path="/" exact component={ListStudent} />
+        <Route path="/edit/:id" component={EditStudent} />
+        <Route path="/create" component={CreateStudent} />
+        <Route path="/delete/:id" component={DeleteStudent} />
+      </div>
+    </Router>
   );
 }
 
